2368ecfecf1c07a338d1b347e176fb443ffab436
Quinton <dropatuningmetal@gmail.com>
2023-08-14 17:19:21 -0700

    basic func

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def to_post_endpoint(url) -> tuple(str, str):
+def to_post_endpoint(url) -> tuple:
     endpoints = ["wp-json/wp/v2/posts", "wp-json/wp/v2/pages"]
 
     # concatenate url with endpoint in URL format
@@ -10,7 +10,17 @@ def get_all_nau_sites() -> list:
     NAU = "https://nau.edu/coe/wp-json/enterprise/v1/site-list"
 
     import requests
-    return requests.get(IN_DOT_NAU).json()['url'] + requests.get(NAU).json()['url']
+    in_sites = requests.get(IN_DOT_NAU)
+    in_sites.encoding = 'utf-8-sig'
+    in_sites = in_sites.json()
+
+    nau_sites = requests.get(NAU)
+    nau_sites.encoding = 'utf-8-sig'
+    nau_sites = nau_sites.json()
+
+    urls = [site['url'] for site in nau_sites] + [site['url'] for site in in_sites]
+
+    return urls
 
 def get_posts(endpoint: str) -> list:
     per_page = 15
@@ -18,18 +28,21 @@ def get_posts(endpoint: str) -> list:
     json_collection = []
 
     while True:
-        arg_str = endpoint + "?per_page=" + str(per_page) + "&offset=" + str(offset) if offset > 0 else ""
+        arg_str = "https://" + endpoint + "?per_page=" + str(per_page)  + (f"&offset={str(offset)}" if offset > 0 else '')
         import requests
-        json = requests.get(arg_str).json()
-        if len(json) > 0:
-            json_collection.append(json)
+        json_data = requests.get(arg_str)
+        json_data.encoding = 'utf-8-sig'
+        json_data = json_data.json()
+
+        if len(json_data) > 0:
+            json_collection = json_collection + [data['link'] for data in json_data]
             offset += per_page
         else:
             break
 
     return json_collection
 
-def find_selector(url: str, selector: str) -> tuple(str, int):
+def find_selector(url: str, selector: str) -> tuple:
     post, page = to_post_endpoint(url)
     posts = get_posts(post)
     pages = get_posts(page)
@@ -40,9 +53,17 @@ def find_selector(url: str, selector: str) -> tuple(str, int):
     import requests
 
     for post in all_posts:
-        pass
+        
+        # get html from post and check if selector is in html
+        html = requests.get(post)
+        html.encoding = 'utf-8-sig'
+        html = html.text
+        soup = BeautifulSoup(html, 'html.parser')
 
+        selections = soup.select(selector)
 
+        if selections:
+            return (post, len(selections))
 
 
 def main():
